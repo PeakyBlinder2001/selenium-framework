114f79511656cf45b12842e95845a06f6c7e8e6e
PeakyBlinder2001 <sumedhgawali8@gmail.com>
2026-02-08 14:08:19 +0530

    Initial Selenium framework

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -1,30 +1,18 @@
-package tests.base;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
 public class BaseTest {
 
-    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-    
+    protected static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     @BeforeMethod
     public void setUp() {
-        driver.set(new ChromeDriver());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver.set(new ChromeDriver(options));
         getDriver().manage().window().maximize();
         getDriver().get("https://www.google.com");
-        ChromeOptions options = new ChromeOptions();
-options.addArguments("--headless=new");
-options.addArguments("--no-sandbox");
-options.addArguments("--disable-dev-shm-usage");
-
-driver = new ChromeDriver(options);
-
     }
-
 
     public WebDriver getDriver() {
         return driver.get();
@@ -36,5 +24,6 @@ driver = new ChromeDriver(options);
             getDriver().quit();
             driver.remove();
         }
+
     }
 }
